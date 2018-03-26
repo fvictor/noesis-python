c693be1c41060ffc212f9d832b925ecfbc087388
Victor Martinez <wertosfv@gmail.com>
2018-03-26 13:24:03 +0200

    Added mocks for doc generation

--- doc/conf.py
+++ doc/conf.py
@@ -160,3 +160,15 @@ texinfo_documents = [
 
 
 # -- Extension configuration -------------------------------------------------
+
+################################################
+
+from mock import Mock as MagicMock
+ 
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+ 
+MOCK_MODULES = ['numpy', 'javabridge']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
